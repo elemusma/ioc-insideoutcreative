450876c9af756c01aadb297e48b43ac250a364e2
Efrain Lemus <efraintlemusm@gmail.com>
2023-03-06 09:55:59 -0700

    start reviews page

--- themes/insideoutcreative/jquery-fittext/fittext.js
+++ themes/insideoutcreative/jquery-fittext/fittext.js
@@ -7,6 +7,8 @@
 // $(".fittext7").fitText(.2);
 // $(".fittext8").fitText(.575);
 // $("#fittext3").fitText(1.1, { minFontSize: '50px', maxFontSize: '75px' });
-$("#home-title").fitText(.625);
-$("#home-subtext").fitText(1.11);
+
+$(".home-title").fitText(1);
+
+// $("#home-subtext").fitText(1.11);
 // alert('hello');
