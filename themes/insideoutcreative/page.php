e7e5d3208d67d5fbd96802279a423ed9273349ea
Efrain Lemus <efraintlemusm@gmail.com>
2023-03-17 11:53:36 -0600

    adjust padding on page

--- themes/insideoutcreative/page.php
+++ themes/insideoutcreative/page.php
@@ -6,10 +6,10 @@ if ( ! post_password_required( $post ) ) {
 
 if(get_the_content()){
 
-echo '<section class="pt-5 pb-5">';
+echo '<section class="" style="padding:150px 0;">';
 echo '<div class="container">';
-echo '<div class="row">';
-echo '<div class="col-md-12">';
+echo '<div class="row justify-content-center">';
+echo '<div class="col-lg-9">';
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 the_content();
 endwhile; else:
